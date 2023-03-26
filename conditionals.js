7c3193f9b29373321da5185343fb7568548e2f44
Ashlee Gudeman <ashleegudeman@gmail.com>
2023-03-25 23:47:54 -0400

    fixed

--- conditionals.js
+++ conditionals.js
@@ -1,13 +1,18 @@
-// prompt user for age
+// prompt user for birth year
 let birthYear = prompt("What year were you born?");
+
+// convert birth year to number
+birthYear = parseInt(birthYear);
+
+// calculate age
 let age = 2023 - birthYear;
 
-// if user was born before 2001 prompt will update with "You are old enough to drink in the US"
+// if user was born before 2002 prompt will update with "You are old enough to drink in the US"
 // if user was born after 2001 prompt will update with "Sorry, you are not old enough to drink in the US"
-// if user was born in 2001 prompt will update with "You are old enough to drink in the US...barely"
-if (age >= 21) {
+// if user was born in 2002 prompt will update with "You are old enough to drink in the US...barely"
+if (birthYear <= 2001) {
     alert("You are " + age + " years old. You are old enough to drink in the US.");
-} else if (age === 21) {
+} else if (birthYear === 2002) {
     alert("You are " + age + " years old. You are old enough to drink in the US...barely.");
 } else {
     alert("You are " + age + " years old. Sorry, you are not old enough to drink in the US.");
@@ -20,13 +25,16 @@ let userChoice = prompt("Choose Heads or Tails.").toLowerCase();
 if (userChoice !== "heads" && userChoice !== "tails") {
     alert("Invalid input. Please choose Heads or Tails.");
 } else {
-// computer chooses "heads or tails" based on 0-1 from Math.floor(Math.random())
-  let computerChoice = Math.floor(Math.random() * 2) === 0 ? "Heads" : "Tails";
+    // computer chooses "heads or tails" based on 0-1 from Math.floor(Math.random())
+    let computerChoice = Math.floor(Math.random() * 2) === 0 ? "heads" : "tails";
 
-// computer will update prompt with "you guessed right, the coinflip landed on [choice]" or "Sorry, the coinflip landed on [choice]
-if (userChoice === computerChoice.toLowerCase()) {
-    alert("You guessed right! The coin flip landed on " + userChoice);
-} else {
-    alert("Sorry, the coin flip landed on " + computerChoice);
-}
+    // convert userChoice to lowercase before comparing
+    userChoice = userChoice.toLowerCase();
+
+    // computer will update prompt with "you guessed right, the coinflip landed on [choice]" or "Sorry, the coinflip landed on [choice]
+    if (userChoice === computerChoice) {
+        alert("You guessed right! The coin flip landed on " + userChoice);
+    } else {
+        alert("Sorry, the coin flip landed on " + computerChoice);
+    }
 }
